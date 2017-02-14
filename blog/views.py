7597e2d76ef2bdea252dce0a9cb64b131532acb1
lim <haan2520@gmail.com>
2017-02-14 13:57:26 +0900

    changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
+from Tools.scripts.highlight import default_latex_document
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+
